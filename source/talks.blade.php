d6db08a942056e559bf654323b64f76d09bde1ed
Rissa <charissa.jackson928@gmail.com>
2023-02-18 21:31:18 -0600

    Add blade for Talks section

---
+++ source/talks.blade.php
@@ -0,0 +1,15 @@
+---
+title: Talks
+description: A little bit about the site
+---
+@extends('_layouts.main')
+
+@section('body')
+    <h1>Talks</h1>
+
+    <img src="/assets/img/about.png"
+        alt="About image"
+        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
+
+    <p class="mb-6"></p>
+@endsection
