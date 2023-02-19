69462f5add6ec85464de432b3d3a7ef03fcae577
Rissa <charissa.jackson928@gmail.com>
2023-02-18 21:29:06 -0600

    Update the About section

--- source/about.blade.php
+++ source/about.blade.php
@@ -5,15 +5,15 @@ description: A little bit about the site
 @extends('_layouts.main')
 
 @section('body')
-    <h1>About</h1>
+    <h1>Rissa Jackson</h1>
 
-    <img src="/assets/img/about.png"
+    <img src="/assets/img/RissaJ.jpeg"
         alt="About image"
         class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
+    <p class="mb-6">I am a Fullstack Software Engineer. I work with Laravel (PHP) and Vue (JavaScript). I'm grateful for all that programming has taught me about problem solving, and love to share what I learn with others. Here is my GitHub: https://github.com/rissajackson</p>
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
+<p class="mb-6">Outside of Software Development I also helped found the 501c3 non-profit Austin Acro Advocates where I served as President for over 2 years. I serve on various committees including the Web, Promotions, Outreach, and Consent & Safety committees. In a previous chapter, I lived in China for two years and taught English. I appreciate all that living abroad taught me.
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
+In my free time you can generally find me doing handstands, partner acrobatics, or dancing.</p>
 @endsection
