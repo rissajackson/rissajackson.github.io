0987a1859c2e5761e733db51b387bb578f5f72c9
Rissa <charissa.jackson928@gmail.com>
2023-04-02 13:47:36 -0500

    Hide blog section

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -1,11 +1,11 @@
 <nav id="js-nav-menu" class="w-auto px-2 pt-6 pb-2 bg-gray-200 shadow hidden lg:hidden">
     <ul class="my-0">
         <li class="pl-4">
-            <a
+            {{-- <a
                 title="{{ $page->siteName }} Blog"
                 href="/blog"
                 class="block mt-0 mb-4 text-sm no-underline {{ $page->isActive('/blog') ? 'active text-blue-500' : 'text-gray-800 hover:text-blue-500' }}"
-            >Blog</a>
+            >Blog</a> --}}
         </li>
         <li class="pl-4">
             <a
--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -1,8 +1,8 @@
 <nav class="hidden lg:flex items-center justify-end text-lg">
-    <a title="{{ $page->siteName }} Blog" href="/blog"
+    {{-- <a title="{{ $page->siteName }} Blog" href="/blog"
         class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/blog') ? 'active text-blue-600' : '' }}">
         Blog
-    </a>
+    </a> --}}
 
     <a title="{{ $page->siteName }} About" href="/about"
         class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/about') ? 'active text-blue-600' : '' }}">
