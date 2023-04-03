5831266ae2b83230b417c2f6674fee20f14e3414
Rissa <charissa.jackson928@gmail.com>
2023-04-03 11:55:50 -0500

    Lighten the gray background

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -25,7 +25,7 @@
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
 
-    <body class="flex flex-col justify-between min-h-screen bg-luckyGrey-200 text-gray-800 leading-normal font-sans">
+    <body class="flex flex-col justify-between min-h-screen bg-luckyGrey-100 text-gray-800 leading-normal font-sans">
         <header class="flex items-center shadow bg-white border-b h-24 py-4" role="banner">
             <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
                 <div class="flex items-center">
