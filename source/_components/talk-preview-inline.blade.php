8c2784a9b84b30e914b767110100f06bef48f86b
Rissa <charissa.jackson928@gmail.com>
2024-04-04 20:37:16 -0500

    Change icon size

--- source/_components/talk-preview-inline.blade.php
+++ source/_components/talk-preview-inline.blade.php
@@ -23,11 +23,11 @@
     </div>
     <div class="w-168">
         @if ($talk->icon_image)
-            <img src="{{ $talk->icon_image }}" alt="{{ $talk->title }} cover image" class="rounded-full object-contain bg-contain mx-auto min-w-52 min-h-52 max-w-56 max-h-56 md:float-right my-6 md:ml-10">
+            <img src="{{ $talk->icon_image }}" alt="{{ $talk->title }} cover image" class="rounded-full object-contain mx-auto min-w-52 min-h-52 max-w-52 max-h-52 md:float-right my-6 md:ml-10">
         @else
             <img src="/assets/img/about.png"
                 alt="About image"
-                class="rounded-full w-168 object-contain bg-contain mx-auto max-w-56 max-h-56 md:float-right my-6 md:ml-10">
+                class="rounded-full w-168 object-contain bg-contain mx-auto max-w-52 max-h-52 md:float-right my-6 md:ml-10">
         @endif
     </div>
 </div>
